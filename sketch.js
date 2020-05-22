fe12d660b14643a9810b183d347928c3f75920d5
pabloqb2000 <qb.pablo.2000@gmail.com>
2020-05-23 00:35:15 +0200

    Started and small UI update

--- sketch.js
+++ sketch.js
@@ -1,33 +1,54 @@
-let sld;
-let btn;
-let tggl;
-let cPicker;
+let nSymmetriesSld;
+let thicknessSld;
+let colorPicker;
+let onlyRotateBtn;
+let showLinesBtn;
+let clearBtn;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(32);
-	sld = new Slider(start=0, end=255, value=32, 0, 0, 100, 10, null, "Background");
-	btn = new Button(x=0, y=0, width=100, height=20, "Reset", resetValue);
-	tggl = new ToggleButton(0,0,100,20,"Discrete", discretice);
-	cPicker = new ColorPicker(0,0, 100, 10, 20);
+
+	// Create UI elements
+	nSymmetriesSld = new Slider(start=2, end=24, value=6, 0, 0, width/12, height/60, 1, "Symmetries", true, 0);
+	thicknessSld = new Slider(start=1, end=30, value=3, 0, 0, width/12, height/60, null, "Thickness");
+	colorPicker = new ColorPicker(0,0, width/12, height/60);
+	onlyRotateBtn = new ToggleButton(0,0, width/12, height/30, "Only rotate");
+	showLinesBtn = new ToggleButton(0,0, width/12, height/30, "Show lines");
+	clearBtn = new Button(0,0, width/12, height/30, "Clear", clearFrame);
+
+	// Start UI
 	UI.tableWidth = 1;
-	UI.tableHeight = 6;
+	UI.tableHeight = 100;
 	UI.distrubute();
+	clearFrame();
 }
 
 function draw() {
-	background(sld.value);
+	// Draw UI
+	// Redraw the frame only in the UI part
+	let w = nSymmetriesSld.getWidth() + UI.widthMargin*2 + width/32;
+	fill(32);
+	rect(0,0, w, height);
+	stroke(100);
+	strokeWeight(1);
+	line(w, 0, w, height);
 	UI.update();
 	UI.draw();
-	translate(width/2, height/2);
+	translate(width/2 + width/12, height/2);
+	scale(1,-1);
 }
 
-function resetValue() {
-	sld.value = 32;
-}
-
-function discretice() {
-	sld.step = (sld.step == null ? 20 : null);
+function clearFrame() {
+	background(32);
+	if(showLinesBtn.active) {
+		let r = createVector(width/2, height/2).mag();
+		strokeWeight(1);
+		stroke(128);
+		for(let i = 0; i < nSymmetriesSld.value; i++) {
+			line(0,0,);
+		}
+	}
 }
 
 function mouseDragged() {
