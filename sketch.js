fc29a4e8a1c3e9707a5096f5e00cedfb3efbc763
pabloqb2000 <qb.pablo.2000@gmail.com>
2020-05-23 01:46:25 +0200

    Now working

--- sketch.js
+++ sketch.js
@@ -2,53 +2,93 @@ let nSymmetriesSld;
 let thicknessSld;
 let colorPicker;
 let onlyRotateBtn;
-let showLinesBtn;
+
 let clearBtn;
+
+let lastPoint;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(32);
 
 	// Create UI elements
-	nSymmetriesSld = new Slider(start=2, end=24, value=6, 0, 0, width/12, height/60, 1, "Symmetries", true, 0);
-	thicknessSld = new Slider(start=1, end=30, value=3, 0, 0, width/12, height/60, null, "Thickness");
-	colorPicker = new ColorPicker(0,0, width/12, height/60);
-	onlyRotateBtn = new ToggleButton(0,0, width/12, height/30, "Only rotate");
-	showLinesBtn = new ToggleButton(0,0, width/12, height/30, "Show lines");
-	clearBtn = new Button(0,0, width/12, height/30, "Clear", clearFrame);
+	nSymmetriesSld = new Slider(start=0, end=24, value=6, 0, 0, width/12, height/40, 1, "Symmetries", true, 0);
+	thicknessSld = new Slider(start=1, end=40, value=3, 0, 0, width/12, height/40, null, "Thickness");
+	colorPicker = new ColorPicker(0,0, width/12, height/40);
+	onlyRotateBtn = new ToggleButton(0,0, width/8, height/20, "Only rotate", null, false);
+	clearBtn = new Button(0,0, width/8, height/20, "Clear", clearFrame);
 
 	// Start UI
 	UI.tableWidth = 1;
 	UI.tableHeight = 100;
 	UI.distrubute();
 	clearFrame();
+
+	// Initial conditions
+	lastPoint = null;
 }
 
 function draw() {
 	// Draw UI
 	// Redraw the frame only in the UI part
-	let w = nSymmetriesSld.getWidth() + UI.widthMargin*2 + width/32;
+	let uiWidth = nSymmetriesSld.getWidth() + UI.widthMargin*2 + width/32;
 	fill(32);
-	rect(0,0, w, height);
+	noStroke();
+	rect(0,0, uiWidth, height);
+	// Sparator from UI
 	stroke(100);
 	strokeWeight(1);
-	line(w, 0, w, height);
+	line(uiWidth, 0, uiWidth, height);
 	UI.update();
 	UI.draw();
-	translate(width/2 + width/12, height/2);
-	scale(1,-1);
+
+	// If mouse is pressed draw the new line
+	if(mouseIsPressed && lastPoint != null) {
+		translate(width/2 + width/12, height/2);
+		scale(1,-1);
+
+		strokeWeight(thicknessSld.value);
+		colorMode(HSL, 1);
+		stroke(colorPicker.getColor());
+		colorMode(RGB, 255);
+
+		// Draw first line
+		let newPoint = screen2Drawing(createVector(mouseX, mouseY));
+		line(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+
+		// Draw the copies of the lines
+		let n = nSymmetriesSld.value;
+		if(n > 0) {
+			for(let i = 0; i < n; i++) {
+				rotate(2*PI/n);
+				line(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+				if(!onlyRotateBtn.active){
+					line(lastPoint.x, -lastPoint.y, newPoint.x, -newPoint.y);
+				}
+			}
+			
+		}
+
+		lastPoint = newPoint;
+	} else if (mousePressed){
+		lastPoint = screen2Drawing(createVector(mouseX, mouseY));
+	} else {
+		lastPoint = null;
+	}
+}
+
+/** 
+ * @param v Coordinates to convert from 
+ * 
+ * @return The given coordinates translated to the drawing coordinates
+ */
+function screen2Drawing(v){
+	return createVector(v.x - width/2 - width/12, v.y*-1 + height/2);
 }
 
 function clearFrame() {
 	background(32);
-	if(showLinesBtn.active) {
-		let r = createVector(width/2, height/2).mag();
-		strokeWeight(1);
-		stroke(128);
-		for(let i = 0; i < nSymmetriesSld.value; i++) {
-			line(0,0,);
-		}
-	}
+	lastPoint = null;
 }
 
 function mouseDragged() {
@@ -59,8 +99,8 @@ function mousePressed() {
 	UI.mouseClicked();
 }
 
-// function keyPressed() {
-//   if(keyCode === 83){
-//
-//   }
-// }
+function keyPressed() {
+  if(keyCode === 82){ // R
+	clearFrame();
+  }
+}
